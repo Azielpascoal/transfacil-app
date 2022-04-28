c466866408347c3a1edd9ac64537335326517036
Aziel Pascoal <azielpascoalmuanzaantonio@gmail.com>
2022-04-28 23:48:09 +0100

    configuração da api o app

---
+++ src/service/api.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+export const BASE_URL =''
+
+
+
+//'http://api.jabakule.com/api http://192.168.100.89/api
+export default {
+
+    // checkToken: async (token) => {
+    //     const req = await fetch(`${BASE_URL}/auth/refresh`, {
+    //         method: 'POST',
+    //         headers: {
+    //             Accept: 'application/json',
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({token})
+    //     });
+    //     const json = await req.json();        
+    //     return json;
+    // },
+    signIn: async (email, password) => {
+        const req = await fetch(`${BASE_URL}/login`, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email,password})
+        });
+        const json = await req.json();        
+        return json;
+    },
+    signUp: async () => {
+        const req = await fetch(`${BASE_URL}/`, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({no})
+        });
+        const json = await req.json();        
+        return json;
+    },  
+
+    senderActivos:async(token)=>{
+        const req = await fetch(`${BASE_URL}/sender_id_activos?token=${token}`,{
+            method:'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            }
+
+        })
+        const json = await req.json();
+        return json;
+    },
+    
+    
+};
