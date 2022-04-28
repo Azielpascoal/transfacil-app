7d0f60e96d65a2a0522cb8c2fc6a357ea2717260
Aziel Pascoal <azielpascoalmuanzaantonio@gmail.com>
2022-04-28 23:48:29 +0100

    signIn:fase cliente

--- src/client/screens/loginScreens/SignIn/index.js
+++ src/client/screens/loginScreens/SignIn/index.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import {Container,InfoArea,InputArea,LoginText,Text,TextB,Button,Button1,ImageLogin,TextC,Text1} from "./style";
 import AppStyle from "../../../../AppStyle"
 import Input from "../../../../components/Input/input"
+import API from "../../../../service/api"
+import {Alert} from "react-native"
 
 export default()=>{
+        const [email,setEmail]= useState()
+        const [password,setPassword]= useState()
+
+        const onSignIn =async()=>{
+           try {
+            if(email != "" || password.lenght >4){
+                let json = await API.signIn(email,password);
+                if(json){
+
+                }
+                else{
+                    
+                }
+            }
+            else{
+
+            }
+               
+           } catch (error) {
+               
+           }
+        }
     return(
         <Container>
             <ImageLogin source={AppStyle.imageSet.LoginIm}/>
